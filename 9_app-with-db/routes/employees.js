78a76151951a449373334b70ce3af39088e23b32
manishvbn <manishvbn@gmail.com>
2023-04-09 15:59:24 +0530

    App with Database Completed

--- 9_app-with-db/routes/employees.js
+++ 9_app-with-db/routes/employees.js
@@ -5,4 +5,18 @@ const router = express.Router();
 
 router.get("/", empCtrl.index);
 
+router.get("/details/:rid", empCtrl.details);
+
+router.get("/create", empCtrl.create_get);
+
+router.post("/create", empCtrl.create_post);
+
+router.get("/edit/:rid", empCtrl.edit_get);
+
+router.post("/edit/:rid", empCtrl.edit_post);
+
+router.get("/delete/:rid", empCtrl.delete_get);
+
+router.post("/delete/:rid", empCtrl.delete_post);
+
 module.exports = router;
